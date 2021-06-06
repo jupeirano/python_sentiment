058b2d228b270df64586eba03f010b7891c80262
Alfredo L'huissier <dds_brigido@hotmail.com>
2021-06-05 23:28:58 -0400

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# AV old
-consumer_key = "MRQaUMosZoSc64zr4UQfdCPWC"
-consumer_secret = "au3zGiRWeBcGY6hZ7ehLQUb7c0XZaEhGxEeltewTNAP0KlfjTA"
-access_token = "2371424485-4ov7NdvVp5CgW2mZlyM9WmwkAiB9Ka0qvGftXw9"
-access_token_secret = "81Z4ebfIg1d2m3ubxJVUNt4Icl0dEzeFM3Zvq2hAdyXBK"
-
-# Next
